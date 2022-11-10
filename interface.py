7e13e85d08ce6b06ab5cf1c36b04820f7b6a154c
silanoc <silanoc@gmail.com>
2022-11-10 11:04:26 +0100

    docstring

--- interface.py
+++ interface.py
@@ -4,12 +4,11 @@
 import questionary
 import turing
 
-def lancer_le_jeu()-> None:
-    print("bienvenu sur ce simulateur de machine de turing")
-    print("----------------------")
-    choisir_et_lancer()
+"""interface en mode console de la machine de Turing
+By silanoc, 10 novembre 2022"""
 
 def choisir_la_machine()-> str:
+    """quesionnaire pour choisir la macine à lancer"""
     choix_machine = questionary.select("Quelle machine voulez-vous utiliser ?",
                                        choices = ["doubler les un", "ajouter 1",
                                               "une autre via un fichier yaml personnalisé"]).ask()
@@ -18,11 +17,23 @@ def choisir_la_machine()-> str:
     elif choix_machine == "ajouter 1":
         fichier = "ajouter1.yaml"
     else:
-        print("pas ok, par défaut ajouter 1")
-        fichier = "ajouter1.yaml"
+        fichier = proposer_un_fichier()
     return fichier
 
+def proposer_un_fichier()-> str:
+    """demande à l'utilisateur le chemin d'un fichier.
+    aucun test de vérification n'est fait !
+    Ce n'est pas bien, mais pour un simple test, je me l'autorise.
+    todo : mettre un test d'existance du fichier
+    """
+    chemin = questionary.text("veuillez entre le chemin du fichier").ask()
+    return chemin
+
 def executer_la_machine(chemin: str)-> None:
+    """création d'une instance de machine,
+    affiche les parametre
+    affiche les étapes successifs
+    """
     machine = turing.creer_une_machine_via_yaml(chemin)
     print("___affichage_initialisation___")
     print(machine.listedetat)
@@ -32,10 +43,12 @@ def executer_la_machine(chemin: str)-> None:
     voulez_vous_continuez()
 
 def choisir_et_lancer()-> None:
+    """simple combinaison pour éviter un doublon de code"""
     la_machine_choisie: str = choisir_la_machine()
     executer_la_machine(la_machine_choisie)
 
 def voulez_vous_continuez()-> None:
+    """questionnaire pour continuer ou quitter"""
     choix_continuer = questionary.select("voulez vous relancer une machine ou quitter",
                                          choices = ["relancer une machine",
                                                     "quitter le programme"]).ask()
@@ -44,7 +57,18 @@ def voulez_vous_continuez()-> None:
     elif choix_continuer == "quitter le programme":
         quitter()
 
+def lancer_le_jeu()-> None:
+    """le début du programme"""
+    print("bienvenu sur ce simulateur de machine de turing")
+    print("----------------------")
+    choisir_et_lancer()
+
 def quitter()-> None:
+    """pour quitter proprement le programme"""
     print("Merci d'avoir utilisé mon programme. A une prochaine fois.")
     exit()
+
+
+if __name__ == "__main__":
+    lancer_le_jeu()
         
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+"""Runner du programme machine de Turing
+By silanoc le 10 novembre 2022"""
+
 import interface
 
 interface.lancer_le_jeu()
